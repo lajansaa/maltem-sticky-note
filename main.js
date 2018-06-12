8142a77ca73706ffb9866837d86438fc5932e3f1
Isa <lajansa@hotmail.com>
2018-06-13 01:04:57 +0800

    order cards by order of creation

--- main.js
+++ main.js
@@ -3,14 +3,22 @@ const addButtonElement = document.getElementById("add-button");
 const searchElement = document.getElementById("search");
 let nextNoteId;
 
+// displays notes when first load or when user searches
 const display = (searchTerm) => {
+  // clear all notes
   while (notesWrapperElement.firstChild) {
     notesWrapperElement.removeChild(notesWrapperElement.firstChild);
   }
-
+  // filter out only sticky notes (sn) related key-value pairs
+  // sort by order of creation (i.e. ascending index)
   const keysArr = Object.keys(localStorage).filter((key) => {
-    return key.slice(0,3) == ("sn-")
-  });
+                                              return key.slice(0,3) == ("sn-")
+                                            })
+                                           .sort((e1, e2) => { 
+                                              const first = parseInt(e1.slice(3));
+                                              const second = parseInt(e2.slice(3))
+                                              return first - second;
+                                            });
   
   if (keysArr.length == 0) {
     nextNoteId = 1;
@@ -19,6 +27,7 @@ const display = (searchTerm) => {
     nextNoteId = lastId + 1;
     keysArr.map(function(key) {
                   const objValue = JSON.parse(localStorage.getItem(key));
+                  // display all notes if searchTerm is undefined
                   if (searchTerm == undefined || objValue.noteTitle == searchTerm) {
                     append(key.slice(3), objValue.noteTitle, objValue.notes)
                   }
@@ -49,11 +58,13 @@ const remove = (id) => {
   localStorage.removeItem("sn-" + id);
 }
 
+// appends notes on initial load or when user saves new notes
 const append = (id, noteTitle, notes) => {
+  // standard noteWrapper template
   const notesWrapper = `<div id="notes-wrapper-${id}" class="notes-wrapper">
                          <div class="content-wrapper">
-                          <input id="title-${id}" class="notes-title" value="${noteTitle}"></br>
-                          <textarea autofocus id="notes-${id}" class="notes" rows="5">${notes}</textarea>
+                          <input id="title-${id}" class="notes-title" value="${noteTitle}" placeholder="title..."></br>
+                          <textarea autofocus id="notes-${id}" class="notes" rows="5" placeholder="notes...">${notes}</textarea>
                          </div>
                          <div class="controls-wrapper">
                           <button id="save-button" onclick="save(${id})">Save</button>
